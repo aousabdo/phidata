fe70cfbecaaf9914d9227e127ca03fe2115b415a
Aous Abdo <aousabdo@FWDTF644CG2HFL.local>
2024-05-26 15:48:58 -0400

    modified the files to save output to md

--- cookbook/assistants/tothemoon.py
+++ cookbook/assistants/tothemoon.py
@@ -1,8 +1,18 @@
 from phi.assistant import Assistant
 from phi.llm.openai import OpenAIChat
 
+from pathlib import Path
+from textwrap import dedent
+
+cwd = Path(__file__).parent.resolve()
+scratch_dir = cwd.joinpath("scratch")
+if not scratch_dir.exists():
+    scratch_dir.mkdir(exist_ok=True, parents=True)
+
 assistant = Assistant(
     llm=OpenAIChat(model="gpt-4o"),
     description="You are a rocket scientist",
+    show_tool_calls=True,
+    save_output_to_file=str(scratch_dir.joinpath("tothemoon.md")),
 )
 assistant.print_response("write a plan to go to the moon stp by step", markdown=True)
