b6c44c3e2b00fecdcbd35924df0f82e7221fdce0
Aous Abdo <aousabdo@FWDTF644CG2HFL.local>
2024-05-26 11:16:03 -0400

    changed files to use llama3

--- cookbook/assistants/cli.py
+++ cookbook/assistants/cli.py
@@ -1,5 +1,16 @@
 from phi.assistant import Assistant
 from phi.tools.duckduckgo import DuckDuckGo
+from phi.llm.openai import OpenAIChat
+from phi.llm.ollama import Ollama
 
-assistant = Assistant(tools=[DuckDuckGo()], show_tool_calls=True, read_chat_history=True)
+# llm = OpenAIChat(model="gpt-4o")
+llm = Ollama(model="llama3")
+
+assistant = Assistant(
+    llm=llm,
+    tools=[DuckDuckGo()], 
+    instructions=["respond to user messages given the tools you have access to"],
+    show_tool_calls=True, 
+    read_chat_history=True)
+
 assistant.cli_app(markdown=True)
--- cookbook/assistants/joke.py
+++ cookbook/assistants/joke.py
@@ -1,6 +1,18 @@
+import argparse
 from phi.assistant import Assistant
 from phi.llm.openai import OpenAIChat
+from phi.llm.ollama import Ollama
 
-topic = "ice cream"
-assistant = Assistant(llm=OpenAIChat(model="gpt-3.5-turbo"))
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Get a joke about a specified topic.")
+parser.add_argument('topic', type=str, help='The topic for the joke')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Get the topic from the command-line arguments
+topic = args.topic
+
+# assistant = Assistant(llm=OpenAIChat(model="gpt-3.5-turbo"))
+assistant = Assistant(llm=Ollama(model='llama3'))
 assistant.print_response(f"Tell me a joke about {topic}")
